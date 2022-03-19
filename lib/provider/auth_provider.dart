7b1a2fa21d37a8e9bcef0e6d06a9e2f1212cbd19
Skrom70 <skrom70@gmail.com>
2022-03-19 19:19:59 +0200

    origin/main: short bag fix

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
-class AuthProvider extends ChangeNotifier {
+class AuthProvider {
   final firebaseAuth = FirebaseAuth.instance;
 
   Future<String?> signUp(
