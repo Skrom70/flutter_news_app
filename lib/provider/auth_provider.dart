08aac460ddae9b5d5bff94bb4baf9241cfff124e
Skrom70 <skrom70@gmail.com>
2022-03-19 23:34:59 +0200

    origin/main: migration to new future handling error

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,50 +1,56 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class AuthProvider {
   final firebaseAuth = FirebaseAuth.instance;
 
-  Future<String?> signUp(
-      {required String email, required String password}) async {
+  Future<void> signUp({required String email, required String password}) async {
     try {
       await firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
-      return null;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
-        return 'The password provided is too weak.';
+        throw ErrorHint('The password provided is too weak.');
       } else if (e.code == 'email-already-in-use') {
-        return 'The account already exists for that email.';
+        throw ErrorHint('The account already exists for that email.');
       }
-      return e.message;
+      throw ErrorHint(e.message ?? 'An error has occurred');
+    } catch (e) {
+      ErrorHint('An error has occurred');
     }
   }
 
-  Future<String?> signIn(
-      {required String email, required String password}) async {
+  Future<void> signIn({required String email, required String password}) async {
     try {
       await firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
-      return null;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
-        return 'No user found for that email.';
+        throw ErrorHint('No user found for that email.');
       } else if (e.code == 'wrong-password') {
-        return 'Wrong password provided for that user.';
+        throw ErrorHint('Wrong password provided for that user.');
       }
-      return e.message;
+      throw ErrorHint(e.message ?? 'An error has occurred');
+    } catch (e) {
+      ErrorHint('An error has occurred');
     }
   }
 
-  Future<String?> resetPassword(String email) async {
+  Future<void> resetPassword(String email) async {
     try {
       await firebaseAuth.sendPasswordResetEmail(email: email);
-      return null;
     } on FirebaseAuthException catch (e) {
-      return e.message;
+      throw ErrorHint(e.message ?? 'An error has occurred');
+    } catch (e) {
+      ErrorHint('An error has occurred');
     }
   }
 
   Future<void> signOut() async {
-    await FirebaseAuth.instance.signOut();
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      throw ErrorHint('An error has occurred');
+    }
   }
 }
